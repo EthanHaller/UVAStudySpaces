43a85bd6346713428e55c9e4e6e57f94d5ad88d0
LukeCreech <lukecreech11@gmail.com>
2023-10-07 22:46:39 -0400

    Admin login
    Added separate page for admin logins

--- study_spaces/views.py
+++ study_spaces/views.py
@@ -3,11 +3,14 @@ from django.contrib.auth import logout
 
 
 def home(request):
+    if request.user.is_authenticated:
+        admins = ['lukecreech11@gmail.com']
+        email = request.user.email
+        if email in admins:
+            return render(request, "study_spaces/admin.html")
     return render(request, "study_spaces/homepage.html")
 
 
 def logout_view(request):
     logout(request)
     return redirect("/")
-
-
