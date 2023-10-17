857ee3c6a2e8808905603700cd2e9f27cb462722
EthanHaller <ethan@cargolabs.org>
2023-10-17 18:30:59 -0400

    updates with bootstrap config

--- study_spaces/urls.py
+++ study_spaces/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+
+app_name = "study_spaces"
 urlpatterns = [
     path("", views.home, name="homepage"),
-    path("logout", views.logout_view),
+    path("logout", views.logout_view, name="logout"),
     path('login', views.login_view),
     path('admin', views.admin_view)
 ]
