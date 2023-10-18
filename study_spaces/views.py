245e770c19c83e2b8a1df1418ea597fbb8db3a32
EthanHaller <ethan@cargolabs.org>
2023-10-18 19:57:45 -0400

    added is_admin function

--- study_spaces/views.py
+++ study_spaces/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 
+def is_admin(email):
+    admins = [
+        'lukecreech11@gmail.com',
+        'rqf8pe@virginia.edu'
+    ]
+
+    return email in admins
+
 @login_required(login_url='/study/login')
 def home(request):
-    admins = ['lukecreech11@gmail.com', 'rqf8pe@virginia.edu']
-    email = request.user.email
-    if email in admins:
+    if is_admin(request.user.email):
         return redirect('/study/admin')
     else:
         return render(request, "study_spaces/user.html")
@@ -24,8 +30,8 @@ def login_view(request):
 
 @login_required(login_url='/study/login')
 def admin_view(request):
-	if request.user.is_authenticated:
-		return render(request, "study_spaces/admin.html")
-	else:
-		return redirect('/study/login')
+    if is_admin(request.user.email):
+        return render(request, "study_spaces/admin.html")
+    else:
+        return redirect('/study')
     
