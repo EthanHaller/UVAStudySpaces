4161d053be29a99a07c4768981d0fb28c146759e
Siddharth Premjith <rqf8pe@virginia.edu>
2023-10-21 22:57:41 -0400

    Set up test database, fixed bugs in tests

--- study_spaces/tests.py
+++ study_spaces/tests.py
@@ -7,12 +7,14 @@ from django.urls import reverse
 class StudySpaceTestCase(TestCase):
     def setUp(self):
         StudySpace.objects.create(name = "Rice Hall", address = "85 Engineer's Way, Charlottesville, VA 22903", latitude = 38.03163727267092, longitude = -78.51082838815206 )
-        StudySpace.objects.create(name = "Thornton Stacks A-Wing", Address = "351 McCormick Rd, Charlottesville, VA 22904", latitude = 38.03332547566759, longitude = -78.50945079424223)
+        StudySpace.objects.create(name = "Thornton Stacks A-Wing", address = "351 McCormick Rd, Charlottesville, VA 22904", latitude = 38.03332547566759, longitude = -78.50945079424223)
     def test_model_string(self):
         rice_hall = StudySpace.objects.get(name="Rice Hall")
         thornton = StudySpace.objects.get(name="Thornton Stacks A-Wing")
-        self.assertEqual(rice_hall.get_sentence, "Rice Hall is located at 85 Engineer's Way, Charlottesville, VA 22903")
-        self.assertEqual(thornton.get_sentence, "Thornton Stacks A-Wing is located at 351 McCormick Rd, Charlottesville, VA 22904")
+        rice_sentence = rice_hall.get_sentence()
+        thornton_sentence = thornton.get_sentence()
+        self.assertEqual(rice_sentence, "Rice Hall is located at 85 Engineer's Way, Charlottesville, VA 22903.")
+        self.assertEqual(thornton_sentence, "Thornton Stacks A-Wing is located at 351 McCormick Rd, Charlottesville, VA 22904.")
 
 class StudySpaceViewTest(TestCase):
     def test_redirect(self):
