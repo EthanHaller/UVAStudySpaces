1eb14083af9e13a2ca4fa584abcd174cc4d2cf88
grace-flynn <143114527+grace-flynn@users.noreply.github.com>
2023-10-31 19:06:32 -0400

    Google maps & NavBar testing
    Not yet passing - need to fix the setUp with fake user so we can access site

--- study_spaces/tests.py
+++ study_spaces/tests.py
@@ -2,24 +2,29 @@ from django.test import TestCase
 from study_spaces.models import *
 from django.urls import reverse
 
+
 # Create your tests here.
 
 class StudySpaceTestCase(TestCase):
     def setUp(self):
-        StudySpace.objects.create(name = "Rice Hall", address = "85 Engineer's Way, Charlottesville, VA 22903", latitude = 38.03163727267092, longitude = -78.51082838815206 )
-        StudySpace.objects.create(name = "Thornton Stacks A-Wing", address = "351 McCormick Rd, Charlottesville, VA 22904", latitude = 38.03332547566759, longitude = -78.50945079424223)
+        StudySpace.objects.create(name="Rice Hall", address="85 Engineer's Way, Charlottesville, VA 22903",
+                                  latitude=38.03163727267092, longitude=-78.51082838815206)
+        StudySpace.objects.create(name="Thornton Stacks A-Wing", address="351 McCormick Rd, Charlottesville, VA 22904",
+                                  latitude=38.03332547566759, longitude=-78.50945079424223)
+
     def test_model_string(self):
         rice_hall = StudySpace.objects.get(name="Rice Hall")
         thornton = StudySpace.objects.get(name="Thornton Stacks A-Wing")
         rice_sentence = rice_hall.get_sentence()
         thornton_sentence = thornton.get_sentence()
         self.assertEqual(rice_sentence, "Rice Hall is located at 85 Engineer's Way, Charlottesville, VA 22903.")
-        self.assertEqual(thornton_sentence, "Thornton Stacks A-Wing is located at 351 McCormick Rd, Charlottesville, VA 22904.")
+        self.assertEqual(thornton_sentence,
+                         "Thornton Stacks A-Wing is located at 351 McCormick Rd, Charlottesville, VA 22904.")
+
 
 class StudySpaceViewTest(TestCase):
     def test_redirect(self):
-
-        #reference: https://docs.djangoproject.com/en/4.2/intro/tutorial05/
+        # reference: https://docs.djangoproject.com/en/4.2/intro/tutorial05/
 
         response = self.client.get(reverse("map"))
         response2 = self.client.get(reverse("study_spaces:route"))
@@ -27,3 +32,48 @@ class StudySpaceViewTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response2.status_code, 302)
         self.assertEqual(response3.status_code, 302)
+
+
+class NavigationBarTest(TestCase):
+    # def setUp(self):
+    #     # create a user and log them in so we can access google maps page
+    #     self.user = ????create_user(username='testuser', password='testpassword')
+    #     self.client.login(username='testuser', password='testpassword')
+    def test_home_link(self):
+        response = self.client.get(reverse("study_spaces:route"))
+        self.assertContains(response, 'href="/study/"')
+
+    def test_index_link(self):
+        response = self.client.get(reverse("study_spaces:route"))
+        self.assertContains(response, 'href="/study/index"')
+
+    def test_favorites_link(self):
+        response = self.client.get(reverse("study_spaces:route"))
+        self.assertContains(response, 'href="#"')
+
+    def test_profile_link(self):
+        response = self.client.get(reverse("study_spaces:route"))
+        self.assertContains(response, 'href="/study/profile"')
+
+    def test_submission_link(self):
+        response = self.client.get(reverse("study_spaces:route"))
+        self.assertContains(response, 'href="/study/submission"')
+
+
+class GoogleMapsViewTest(TestCase):
+    # def setUp(self):
+    #     # create a user and log them in so we can access google maps page
+    #     self.user = ????create_user(username='testuser', password='testpassword')
+    #     self.client.login(username='testuser', password='testpassword')
+
+    def test_google_maps_page(self):
+        response = self.client.get(reverse("map"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_google_maps_init(self):
+        response = self.client.get(reverse("map"))
+        self.assertContains(response, "initMap")
+
+    def test_google_maps_api(self):
+        response = self.client.get(reverse("map"))
+        self.assertContains(response, "maps.googleapis.com/maps/api/js?key=")
