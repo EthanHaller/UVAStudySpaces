e104de142fc7e82dec955dc5760cc4746ceb0a85
EthanHaller <ttk4ey@virginia.edu>
2023-11-07 10:10:32 -0500

    added on click transition to homepage

--- static/home_map.js
+++ static/home_map.js
@@ -19,9 +19,14 @@ function initMap() {
 			position: { lat: parseFloat(i.latitude), lng: parseFloat(i.longitude) },
 			map: map,
 		})
-		marker.addListener("click", () => {
-			map.setZoom(18)
-			map.setCenter(marker.getPosition())
+
+		const studySpotClicked = () => {
+			map.setZoom(16)
+			const moveMap = () => {
+				map.setCenter(marker.getPosition())
+				map.setZoom(18)
+			}
+			setTimeout(moveMap, 700)
 
 			modData.forEach((element) => {
 				const card = document.getElementById("study-card-id-" + element.id)
@@ -33,24 +38,12 @@ function initMap() {
 			card.style.backgroundColor = "#F5F5F5"
 			card.style.borderColor = "#8BC34A"
 			card.scrollIntoView({ behavior: 'smooth' })
-		})
+		}
+
+		marker.addListener("click", studySpotClicked)
 		
 		const card = document.getElementById("study-card-id-" + i.id)
-		card.addEventListener("click", () => {
-			map.setZoom(18)
-			map.setCenter(marker.getPosition())
-
-			modData.forEach((element) => {
-				const card = document.getElementById("study-card-id-" + element.id)
-				card.style.backgroundColor = null
-				card.style.borderColor = null
-			})
-
-			const card = document.getElementById("study-card-id-" + i.id)
-			card.style.backgroundColor = "#F5F5F5"
-			card.style.borderColor = "#8BC34A"
-			card.scrollIntoView({ behavior: 'smooth' })
-		})
+		card.addEventListener("click", studySpotClicked)
 	})
 }
 
