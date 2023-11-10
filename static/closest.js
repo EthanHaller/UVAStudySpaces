6ad160242e7f6d183bd5ceecd3352138793c3db9
Siddharth Premjith <rqf8pe@virginia.edu>
2023-11-10 17:17:58 -0500

    Deleted unnecessary closest.js file

--- static/closest.js
+++
@@ -1,8 +0,0 @@
-$.getScript("https://maps.googleapis.com/maps/api/js?key=" + window.key + "&callback=initMap&v=weekly&libraries=places").done(function (script, textStatus) {
-	google.maps.event.addListener(window, "load", showClosest)
-})
-
-function showClosest() {
-    var b = populateStartAddressWithUserLocation()
-}
-
